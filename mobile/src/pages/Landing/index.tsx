08804088e4290fee3cabc000c342706b67c065c5
maluarmini <luiza.armini@gmail.com>
2020-08-08 10:38:50 -0300

    Finished first version <3

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View,Image,Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View,Image,Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {RectButton} from 'react-native-gesture-handler'
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -12,6 +13,14 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 export default function Landing() {
     const navigation = useNavigation();
+    const [totalConnections, setTotalConnections] = useState();
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data
+            setTotalConnections(total);
+        })
+    }, []);
 
     function handleNavigateToGiveClassesPage(){
         navigation.navigate('GiveClasses');
@@ -41,7 +50,7 @@ export default function Landing() {
 
             </View>
 
-        <Text style={styles.totalConnections}>Total de 285 de conexões ja realizadas {''}
+        <Text style={styles.totalConnections}>Total de {totalConnections} de conexões ja realizadas {''}
             <Image source={heartIcon}/>
         </Text>
             
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,12 +1,129 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState} from 'react';
+import { View,ScrollView,Text,TextInput } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import {Feather, AntDesign} from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import styles from './styles';
+
+import api from '../../services/api';
+
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, {Teacher} from '../../components/TeacherItem';
+import { useFocusEffect } from '@react-navigation/native';
+
+
 
 export default function TeacherList(){
+    const [isFilterVisible, setIsFilterVisible] = useState(false);
+    const [subject,setSubject] = useState('');
+    const [week_day,setWeekDay] = useState('');
+    const [time,setTime] = useState('');
+    const [teachers, setTeachers] = useState([]);
+    const [favorites, setFavorites] = useState<number[]>([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if(response) {
+                const favoritedTeachers = JSON.parse(response);
+                const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+                    return teacher.id;
+                })
+
+                setFavorites(favoritedTeachersIds);
+            }
+        });
+    }
+    useFocusEffect(() => {
+        loadFavorites();
+    })
+
+    function handleFiltersSubmit() {
+        loadFavorites();
+        api.get('classes',{
+            params : {
+                    subject,
+                    week_day,
+                    time
+                }
+        }).then(response => {
+            console.log(response.data);
+            setTeachers(response.data)
+        });
+        setIsFilterVisible(false);   
+    }
+
+    function handleIsVisibleFilter() {
+        // if(isFilterVisible === true){
+        //     setIsFilterVisible(false);
+        // }else{
+        //     setIsFilterVisible(true);
+        // }
+        setIsFilterVisible(!isFilterVisible);
+    }
+    
     return(
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis"/>
+            <PageHeader 
+            title="Proffys disponíveis" 
+            headerRight={(
+                <BorderlessButton onPress={handleIsVisibleFilter}>
+                    <Feather name="filter" size={20} color="#fff"/>
+                </BorderlessButton>
+            )}>
+               { isFilterVisible && (<View style={styles.searchForm}>
+                    <Text style={styles.label}>Matéria</Text>
+                    <TextInput 
+                    style={styles.input}
+                    placeholderTextColor="#c1bccc"
+                    placeholder="Qual a matéria?"
+                    value={subject}
+                    onChangeText={text => setSubject(text)}
+                    />
+                    <View style={styles.inputGroup}>
+                        <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Dia da semana</Text>
+                            <TextInput 
+                            style={styles.input}
+                            placeholderTextColor="#c1bccc"
+                            placeholder="Qual o dia?"
+                            value={week_day}
+                            onChangeText={text => setWeekDay(text)}
+                            />
+                        </View>
+
+                        <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Horário</Text>
+                            <TextInput 
+                            style={styles.input}
+                            placeholderTextColor="#c1bccc"
+                            placeholder="Qual horário?"
+                            value={time}
+                            onChangeText={text => setTime(text)}
+                            />
+                        </View>
+                        
+                    </View>
+
+                    <RectButton style={styles.submitButton} onPress={handleFiltersSubmit}>
+                        <AntDesign name="search1" size={24} color="#fff" />
+                        <Text style={styles.submitButtonText}>Filtrar</Text>
+                    </RectButton>
+                </View>)
+                }
+            </PageHeader>
+
+            <ScrollView
+            style={styles.teacherList}
+            contentContainerStyle={{
+                paddingHorizontal: 16,
+                paddingBottom: 16,
+            }}
+            >   
+                {teachers.map((teacher:Teacher) => (
+                    <TeacherItem key={teacher.id} teacher={teacher} favorited={favorites.includes(teacher.id)}/>
+                ))}
+            </ScrollView>
         </View>
     );
 }
